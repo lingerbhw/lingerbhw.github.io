910b9b2421d64fe8cc3d281bef101147eb493f07
lingerbhw <3501737127@qq.com>
2022-08-18 14:42:01 +0800

    Site updated: 2022-08-18 14:42:01

--- js/background.js
+++ js/background.js
@@ -1,1 +1,4 @@
-var full_page=document.getElementsByClassName("full_page");0!=full_page.length&&(full_page[0].style.background="transparent");
+var full_page = document.getElementsByClassName("full_page");
+if (full_page.length != 0) {
+  full_page[0].style.background = "transparent";
+}
