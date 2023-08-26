4d90b084e0170fc75c12d7f14e2e006aafb2e3a6
lingerbhw <3501737127@qq.com>
2023-08-26 21:01:39 +0800

    Site updated: 2023-08-26 21:01:01

--- js/local.js
+++ js/local.js
@@ -1,8 +1,1 @@
-//重定向浏览器地址
-pjax.site_handleResponse = pjax.handleResponse;
-pjax.handleResponse = function(responseText, request, href, options){
-  Object.defineProperty(request,'responseURL',{
-    value: href
-  });
-  pjax.site_handleResponse(responseText,request,href,options);
-}
+pjax.site_handleResponse=pjax.handleResponse,pjax.handleResponse=function(e,n,s,p){Object.defineProperty(n,"responseURL",{value:s}),pjax.site_handleResponse(e,n,s,p)};
